3a7cd6b0b15c3c04e7c0e8913c257b867bf4769e
Paul Robinson <paulalex675@hotmail.com>
2021-04-19 09:40:36 +0100

    hot wire practise

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  after_create_commit { broadcast_prepend_to "posts" }
+  after_update_commit { broadcast_replace_to "posts" }
+  after_destroy_commit { broadcast_remove_to "posts" }
+  validates :body, presence: true
+end
